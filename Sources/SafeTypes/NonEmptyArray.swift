620681ff8455bf4b8c5faba8fbf9a27c4fe57a44
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2024-03-03 02:01:32 -0300

    Add missing methods and update tests

--- Sources/SafeTypes/NonEmptyArray.swift
+++ Sources/SafeTypes/NonEmptyArray.swift
@@ -8,57 +8,287 @@ public struct NonEmptyArray<Element> {
         self.items = items
     }
 
+    public init?<S>(_ elements: S) where S : Sequence, Element == S.Element {
+        let items = Array(elements)
+        guard !items.isEmpty else {
+            return nil
+        }
+        self.items = items
+    }
+
     public init(_ head: Element, _ tail: Element...) {
         self.items = CollectionOfOne(head) + tail
     }
 
-    public init(_ lhs: NonEmptyArray<Element>, _ rhs: NonEmptyArray<Element>) {
-        self.items = lhs.items + rhs.items
+    public init<S>(_ lhs: NonEmptyArray, _ rhs: S) where S : Sequence, Element == S.Element {
+        self.items = lhs.items + rhs
+    }
+
+    public init?(repeating repeatedValue: Element, count: Int) {
+        guard count > 0 else {
+            return nil
+        }
+
+        self.items = Array(repeating: repeatedValue, count: count)
     }
 }
 
 // MARK: - Array
 
 extension NonEmptyArray {
-    public var first: Element {
-        items.first!
+    public var capacity: Int {
+        items.capacity
+    }
+
+    public func withUnsafeBufferPointer<R>(_ body: (UnsafeBufferPointer<Element>) throws -> R) rethrows -> R {
+        try items.withUnsafeBufferPointer(body)
+    }
+
+    public func withUnsafeBytes<R>(_ body: (UnsafeRawBufferPointer) throws -> R) rethrows -> R {
+        try items.withUnsafeBytes(body)
+    }
+
+    public func filter(_ isIncluded: (Element) throws -> Bool) rethrows -> [Element] {
+        try items.filter(isIncluded)
+    }
+
+    public mutating func popLast() -> Element? {
+        guard count > 1 else {
+            return nil
+        }
+
+        return items.popLast()
+    }
+
+    public mutating func removeLast(_ k: Int) {
+        guard k < count else {
+            items.removeLast(count - 1)
+            return
+        }
+
+        items.removeLast(k)
+    }
+
+    public func dropLast(_ k: Int) -> ArraySlice<Element> {
+        items.dropLast(count)
+    }
+
+    public func suffix(_ maxLength: Int) -> ArraySlice<Element> {
+        items.suffix(maxLength)
+    }
+
+    public func dropFirst(_ k: Int = 1) -> ArraySlice<Element> {
+        items.dropFirst(k)
+    }
+
+    public func drop(while predicate: (Element) throws -> Bool) rethrows -> ArraySlice<Element> {
+        try items.drop(while: predicate)
+    }
+
+    public func prefix(_ maxLength: Int) -> ArraySlice<Element> {
+        items.prefix(maxLength)
+    }
+
+    public func prefix(while predicate: (Element) throws -> Bool) rethrows -> ArraySlice<Element> {
+        try items.prefix(while: predicate)
+    }
+
+    public func prefix(upTo end: Int) -> ArraySlice<Element> {
+        items.prefix(upTo: end)
+    }
+
+    public func suffix(from start: Int) -> ArraySlice<Element> {
+        items.suffix(from: start)
+    }
+
+    public func prefix(through position: Int) -> ArraySlice<Element> {
+        items.prefix(through: position)
+    }
+
+    public func split(maxSplits: Int = Int.max, omittingEmptySubsequences: Bool = true, whereSeparator isSeparator: (Element) throws -> Bool) rethrows -> [ArraySlice<Element>] {
+        try items.split(maxSplits: maxSplits, omittingEmptySubsequences: omittingEmptySubsequences, whereSeparator: isSeparator)
+    }
+
+    public mutating func removeFirst() -> Element? {
+        guard count > 1 else {
+            return nil
+        }
+
+        return items.removeFirst()
+    }
+
+    public mutating func removeFirst(_ k: Int) {
+        guard count > k else {
+            removeFirst(count - 1)
+            return
+        }
+
+        return items.removeFirst(k)
     }
 
     public var last: Element {
         items.last!
     }
 
-    public var count: Int {
+    public func firstIndex(where predicate: (Element) throws -> Bool) rethrows -> Int? {
+        try items.firstIndex(where: predicate)
+    }
+
+    public func last(where predicate: (Element) throws -> Bool) rethrows -> Element? {
+        try items.last(where: predicate)
+    }
+
+    public func lastIndex(where predicate: (Element) throws -> Bool) rethrows -> Int? {
+        try items.lastIndex(where: predicate)
+    }
+
+    public mutating func partition(by belongsInSecondPartition: (Element) throws -> Bool) rethrows -> Int {
+        try items.partition(by: belongsInSecondPartition)
+    }
+
+    public func shuffled<T>(using generator: inout T) -> [Element] where T : RandomNumberGenerator {
+        items.shuffled(using: &generator)
+    }
+
+    public mutating func shuffle<T>(using generator: inout T) where T : RandomNumberGenerator {
+        items.shuffle()
+    }
+
+    public var lazy: LazySequence<Array<Element>> {
+        items.lazy
+    }
+
+    public mutating func swapAt(_ i: Int, _ j: Int) {
+        items.swapAt(i, j)
+    }
+
+     public var isEmpty: Bool { false }
+
+     public var first: Element {
+        items.first!
+    }
+
+     public var count: Int {
         items.count
+    }
+
+    public func reversed() -> NonEmptyArray {
+        NonEmptyArray(items.reversed())!
+    }
+    
+    public func index(_ i: Int, offsetBy distance: Int) -> Int {
+        items.index(i, offsetBy: distance)
+    }
+
+    public func index(_ i: Int, offsetBy distance: Int, limitedBy limit: Int) -> Int? {
+        items.index(i, offsetBy: distance, limitedBy: limit)
     }
 
     public mutating func append(_ newElement: Element) {
         items.append(newElement)
     }
 
+    public mutating func append(_ newElements: Element...) {
+        items.append(contentsOf: newElements)
+    }
+
     public mutating func append<S>(contentsOf newElements: S) where S : Sequence, Element == S.Element{
         items.append(contentsOf: newElements)
     }
 
-    public mutating func insert(_ newElement: Element, at i: Int){
-        items.insert(newElement, at: i)
+    public mutating func insert(_ newElement: Element, at index: Int){
+        items.insert(newElement, at: index)
     }
 
-    public mutating func remove(at i: Int) -> Element? {
+    public mutating func remove(at index: Int) -> Element? {
         guard items.count > 1 else {
             return nil
         }
-        return items.remove(at: i)
+
+        guard items.indices.contains(index) else {
+            return nil
+        }
+
+        return items.remove(at: index)
     }
 
-    @inlinable public static func +(lhs: NonEmptyArray, rhs: NonEmptyArray) -> NonEmptyArray {
+    public static func + <S>(lhs: NonEmptyArray, rhs: S) -> NonEmptyArray<Element> where S : Sequence, Element == S.Element {
         NonEmptyArray(lhs, rhs)
     }
 
-    @inlinable public static func +=(lhs: inout NonEmptyArray, rhs: NonEmptyArray) {
+     public static func +=(lhs: inout NonEmptyArray, rhs: NonEmptyArray) {
         lhs.append(contentsOf: rhs.items)
     }
 }
+
+// MARK: - Sequence
+
+extension NonEmptyArray: Sequence {
+     public func makeIterator() -> IndexingIterator<[Element]> {
+        items.makeIterator()
+    }
+}
+
+// MARK: - Collection
+
+extension NonEmptyArray: Collection {
+     public var startIndex: Int {
+        items.startIndex
+    }
+
+     public var endIndex: Int {
+        items.endIndex
+    }
+}
+
+// MARK: - MutableCollection
+
+extension NonEmptyArray: MutableCollection {
+    public subscript(position: Int) -> Element {
+         get {
+            return items[position]
+        }
+        set {
+            return items[position] = newValue
+        }
+    }
+}
+
+// MARK: - BidirectionalCollection
+
+extension NonEmptyArray: BidirectionalCollection {
+
+     public func index(before i: Int) -> Int {
+        items.index(before: i)
+    }
+
+     public func index(after i: Int) -> Int {
+        items.index(after: i)
+    }
+}
+
+// MARK: - CustomStringConvertible
+
+extension NonEmptyArray: CustomStringConvertible {
+    public var description: String {
+        items.description
+    }
+}
+
+// MARK: - Comparable
+
+extension NonEmptyArray: Comparable where Element: Comparable {
+    public static func < (lhs: NonEmptyArray<Element>, rhs: NonEmptyArray<Element>) -> Bool {
+        for (v1, v2) in zip(lhs.items, rhs.items){
+            if v1 != v2 { return v1 < v2 }
+        }
+        return lhs.count < rhs.count
+    }
+}
+
+// MARK: - RandomAccessCollection
+
+extension NonEmptyArray: RandomAccessCollection {}
 
 // MARK: - Equatable
 
@@ -79,43 +309,3 @@ extension NonEmptyArray: Encodable where Element: Encodable {}
 // MARK: - Decodable
 
 extension NonEmptyArray: Decodable where Element: Decodable {}
-
-// MARK: - Sequence
-
-extension NonEmptyArray: Sequence {
-    public func makeIterator() -> IndexingIterator<[Element]> {
-        items.makeIterator()
-    }
-}
-
-// MARK: - Collection
-
-extension NonEmptyArray: Collection {
-    public var startIndex: Int {
-        items.startIndex
-    }
-
-    public var endIndex: Int {
-        items.endIndex
-    }
-
-    public subscript(position: Int) -> Element {
-        items[position]
-    }
-
-    public func index(after i: Int) -> Int {
-        items.index(after: i)
-    }
-
-    public func reversed() -> NonEmptyArray {
-        NonEmptyArray(items.reversed())!
-    }
-}
-
-// MARK: - CustomStringConvertible
-
-extension NonEmptyArray: CustomStringConvertible where Element: CustomStringConvertible {
-    public var description: String {
-        items.description
-    }
-}
--- Tests/SafeTypesTests/NonEmptyArrayTests.swift
+++ Tests/SafeTypesTests/NonEmptyArrayTests.swift
@@ -11,6 +11,16 @@ final class NonEmptyArrayTests: XCTestCase {
         XCTAssertEqual(NonEmptyArray(1, 2).items, [1, 2])
         XCTAssertEqual(NonEmptyArray(1, 2, 3).items, [1, 2, 3])
         XCTAssertEqual(NonEmptyArray(1, 2, 3, 4).items, [1, 2, 3, 4])
+        XCTAssertEqual(NonEmptyArray(NonEmptyArray(1, 2), NonEmptyArray(3, 4)), NonEmptyArray(1, 2, 3, 4))
+    }
+
+    // MARK: - Array
+
+    func test_isEmpty_returnsFalse() {
+        XCTAssertFalse(NonEmptyArray(1).isEmpty)
+        XCTAssertFalse(NonEmptyArray(1, 2).isEmpty)
+        XCTAssertFalse(NonEmptyArray(1, 2, 3).isEmpty)
+        XCTAssertFalse(NonEmptyArray(1, 2, 3, 4).isEmpty)
     }
 
     func test_first_returnsCorrectly() {
@@ -32,6 +42,94 @@ final class NonEmptyArrayTests: XCTestCase {
         XCTAssertEqual(NonEmptyArray(1, 2).count, 2)
         XCTAssertEqual(NonEmptyArray(1, 2, 3).count, 3)
         XCTAssertEqual(NonEmptyArray(1, 2, 3, 4).count, 4)
+    }
+
+    func test_append_appendsItemsCorrectly() {
+        var sut = NonEmptyArray(1)
+
+        sut.append(2)
+        XCTAssertEqual(sut.items, [1, 2])
+
+        sut.append(3, 4)
+        XCTAssertEqual(sut.items, [1, 2, 3, 4])
+
+        sut.append(5, 6, 7)
+        XCTAssertEqual(sut.items, [1, 2, 3, 4, 5, 6, 7])
+
+        sut.append(contentsOf: NonEmptyArray(8, 9))
+        XCTAssertEqual(sut.items, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+    }
+
+    func test_insert_insertsItemsCorrectly() {
+        var sut = NonEmptyArray(1)
+
+        sut.insert(3, at: 1)
+        XCTAssertEqual(sut.items, [1, 3])
+
+        sut.insert(2, at: 1)
+        XCTAssertEqual(sut.items, [1, 2, 3])
+
+        sut.insert(4, at: 3)
+        XCTAssertEqual(sut.items, [1, 2, 3, 4])
+
+        sut.insert(0, at: 0)
+        XCTAssertEqual(sut.items, [0, 1, 2, 3, 4])
+    }
+
+    func test_comparable_correctlyComparesNonEmptyArrays() {
+        XCTAssertFalse(NonEmptyArray(1, 2) < NonEmptyArray(1, 2))
+        XCTAssertFalse(NonEmptyArray(1, 2) > NonEmptyArray(1, 2))
+
+        XCTAssertTrue(NonEmptyArray(1, 2) < NonEmptyArray(2, 1))
+        XCTAssertFalse(NonEmptyArray(1, 2) > NonEmptyArray(2, 1))
+
+        XCTAssertTrue(NonEmptyArray(1) < NonEmptyArray(1, 2))
+        XCTAssertFalse(NonEmptyArray(1) > NonEmptyArray(1, 2))
+
+        XCTAssertFalse(NonEmptyArray(1, 2) < NonEmptyArray(1))
+        XCTAssertTrue(NonEmptyArray(1, 2) > NonEmptyArray(1))
+    }
+
+    func test_remove_returnsCorrectly() {
+        var sut = NonEmptyArray(1,2,3)
+
+        XCTAssertEqual(sut.remove(at: 2), 3)
+        XCTAssertEqual(sut.remove(at: 1), 2)
+        XCTAssertNil(sut.remove(at: 0))
+        XCTAssertNil(sut.remove(at: 0))
+        XCTAssertEqual(sut.items, [1])
+        XCTAssertEqual(sut.count, 1)
+        XCTAssertFalse(sut.isEmpty)
+    }
+
+    func test_remove_whenOutOfBounds_returnsNil() {
+        var sut = NonEmptyArray(1,2,3)
+
+        XCTAssertNil(sut.remove(at: -1))
+        XCTAssertNil(sut.remove(at: 42))
+        XCTAssertEqual(sut.items, [1, 2, 3])
+        XCTAssertEqual(sut.count, 3)
+        XCTAssertFalse(sut.isEmpty)
+    }
+
+    func test_addition_returnsCorrectly() {
+        var sut = NonEmptyArray(1)
+
+        sut += NonEmptyArray(2)
+        XCTAssertEqual(sut, NonEmptyArray(1, 2))
+
+        sut += NonEmptyArray(3)
+        XCTAssertEqual(sut, NonEmptyArray(1, 2, 3))
+
+        XCTAssertEqual(NonEmptyArray(1, 2) + NonEmptyArray(3, 4), NonEmptyArray(1, 2, 3, 4))
+        XCTAssertEqual(NonEmptyArray("Foo") + NonEmptyArray("Bar"), NonEmptyArray("Foo", "Bar"))
+        XCTAssertEqual(NonEmptyArray("Foo") + ["Bar"], NonEmptyArray("Foo", "Bar"))
+    }
+
+    func test_reversed_returnsCorrectly() {
+        XCTAssertEqual(NonEmptyArray(1, 2, 3).reversed(), NonEmptyArray(3, 2, 1))
+        XCTAssertEqual(NonEmptyArray(1, 2, 3).reversed().reversed(), NonEmptyArray(1, 2, 3))
+        XCTAssertEqual(NonEmptyArray(3, 2, 1).reversed(), NonEmptyArray(1, 2, 3))
     }
 
     // MARK: - Sequence
@@ -74,22 +172,40 @@ final class NonEmptyArrayTests: XCTestCase {
         XCTAssertEqual(sum, 6)
     }
 
-    // MARK: - Other
+    // MARK: - Collection
 
-    func test_remove_returnsCorrectly() {
-        var sut = NonEmptyArray(1,2,3)
-
-        XCTAssertEqual(sut.remove(at: 2), 3)
-        XCTAssertEqual(sut.remove(at: 1), 2)
-        XCTAssertNil(sut.remove(at: 0))
-        XCTAssertNil(sut.remove(at: 0))
-        XCTAssertEqual(sut.items, [1])
-        XCTAssertEqual(sut.count, 1)
+    func test_startIndex_returnsZero() {
+        XCTAssertEqual(NonEmptyArray(1).startIndex, 0)
+        XCTAssertEqual(NonEmptyArray(1, 2).startIndex, 0)
+        XCTAssertEqual(NonEmptyArray(1, 2, 3).startIndex, 0)
+        XCTAssertEqual(NonEmptyArray(1, 2, 3, 4).startIndex, 0)
     }
 
-    func test_reversed_returnsCorrectly() {
-        XCTAssertEqual(NonEmptyArray(1, 2, 3).reversed(), NonEmptyArray(3, 2, 1))
-        XCTAssertEqual(NonEmptyArray(1, 2, 3).reversed().reversed(), NonEmptyArray(1, 2, 3))
-        XCTAssertEqual(NonEmptyArray(3, 2, 1).reversed(), NonEmptyArray(1, 2, 3))
+    func test_endIndex_returnsCorrectly() {
+        XCTAssertEqual(NonEmptyArray(1).endIndex, 1)
+        XCTAssertEqual(NonEmptyArray(1, 2).endIndex, 2)
+        XCTAssertEqual(NonEmptyArray(1, 2, 3).endIndex, 3)
+        XCTAssertEqual(NonEmptyArray(1, 2, 3, 4).endIndex, 4)
+    }
+
+    // MARK: - MutableCollection
+
+    func test_subscriptPosition_accessCorrectElement() {
+        var sut = NonEmptyArray(1, 2, 3, 4)
+
+        XCTAssertEqual(sut[0], 1)
+        XCTAssertEqual(sut[1], 2)
+        XCTAssertEqual(sut[2], 3)
+        XCTAssertEqual(sut[3], 4)
+
+        sut[0] = 4
+        sut[1] = 3
+        sut[2] = 2
+        sut[3] = 1
+
+        XCTAssertEqual(sut[0], 4)
+        XCTAssertEqual(sut[1], 3)
+        XCTAssertEqual(sut[2], 2)
+        XCTAssertEqual(sut[3], 1)
     }
 }
